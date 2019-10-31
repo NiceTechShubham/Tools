f047f6fad1e7a18e348205ce7c17d2840ef89a8e
Shubham Kumar <57167757+NiceTechShubham@users.noreply.github.com>
2019-10-31 08:34:31 +0530

    Add files via upload

---
+++ Tools.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# If you want to take credit for this script so you have to look yourself again
+apt update && apt upgrade -y
+pkg install figlet -y
+pkg install php -y
+pkg install ruby -y
+gem install lolcat
+clear
+echo "This code is written by:-" | lolcat
+figlet -f standard Shubham | lolcat
+
+
+pkg install cowsay -y
+apt install openssh -y
+pkg install wget -y
+pkg update 
+pkg install curl
+clear
+echo "Now Metasploit is installing" | lolcat
+pkg install unstable-repo
+pkg install metasploit -y
+clear
+echo "Now metasploit is installed" | lolcat
+echo "Now it's ready to Rock" | lolcat
+clear
+echo "This code is written by:-" | lolcat
+figlet -f banner Shubham | lolcat
+echo "Now You're Ready to Rock" | lolcat
